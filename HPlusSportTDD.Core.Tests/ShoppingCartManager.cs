86f0a394ba04f5df0738d647ddb355cd4ed99cec
Sergei Gorgun <sergei_gorgun@epam.com>
2024-12-05 17:09:46 +0200

    Add shopping cart functionality and related tests
    Updated ShoppingCartManager to include a private list `_shoppingCart` for storing items. Modified `AddToCart` method to add items to this list and return all items in the cart. Added `ShouldReturnArticlesAddedToCart` test in ShoppingCartTests to verify multiple items can be added and returned. Minor formatting change in an existing assertion.

--- HPlusSportTDD.Core.Tests/ShoppingCartManager.cs
+++ HPlusSportTDD.Core.Tests/ShoppingCartManager.cs
@@ -3,15 +3,19 @@ namespace HPlusSportTDD.Core
 {
     internal class ShoppingCartManager
     {
+        private List<AddToCartItem> _shoppingCart;
         public ShoppingCartManager()
         {
+            _shoppingCart = new List<AddToCartItem>();
         }
 
         internal AddToCartResponse AddToCart(AddToCartRequest request)
         {
+            _shoppingCart.Add(request.Item);
+
             return new AddToCartResponse()
             {
-                Items = new AddToCartItem[] { request.Item }
+                Items = _shoppingCart.ToArray()
             };
         }
     }
--- HPlusSportTDD.Core.Tests/ShoppingCartTests.cs
+++ HPlusSportTDD.Core.Tests/ShoppingCartTests.cs
@@ -32,8 +32,44 @@ namespace HPlusSportTDD.Core
 
             AddToCartResponse response = manager.AddToCart(request);
 
-            Assert.NotNull(response);
+            Assert. NotNull(response);
             Assert.Contains(item, response.Items);
+        }
+        
+        [Test]
+        public void ShouldReturnArticlesAddedToCart()
+        {
+            var item1 = new AddToCartItem()
+            {
+                Id = 1,
+                Quantity = 5,
+            };
+
+            var request = new AddToCartRequest()
+            {
+                Item = item1,
+            };
+
+            var manager = new ShoppingCartManager();
+
+            AddToCartResponse response = manager.AddToCart(request);
+
+            var item2 = new AddToCartItem()
+            {
+                Id = 2,
+                Quantity = 15,
+            };
+
+            request = new AddToCartRequest()
+            {
+                Item = item2,
+            };
+
+            response = manager.AddToCart(request);
+
+            Assert. NotNull(response);
+            Assert.Contains(item1, response.Items);
+            Assert.Contains(item2, response.Items);
         }
     }
 }
